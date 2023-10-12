9441f694282bfeb6d78b172523775ae0eb7b8374
Naderab <abdellaouinader@gmail.com>
2023-10-12 11:54:12 +0100

    seance 4 : Doctrine + crud author

--- symfony3a11/src/Controller/AuthorController.php
+++ symfony3a11/src/Controller/AuthorController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
+use App\Repository\AuthorRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,5 +24,33 @@ class AuthorController extends AbstractController
         return $this->render('author/show.html.twig',[
             'n'=>$name
         ]);
+    }
+
+    #[Route('/author/get/all',name:'app_get_all_author')]
+    public function getAll(AuthorRepository $repo){
+        $authors = $repo->findAll();
+        return $this->render('author/listauthors.html.twig',[
+            'a'=>$authors
+        ]);
+    }
+
+    #[Route('/author/add',name:'app_add_author')]
+    public function add(ManagerRegistry $manager){
+
+        $author = new Author();
+        $author->setUsername('author 1');
+        $author->setEmail('author1@esprit.tn');
+        $manager->getManager()->persist($author);
+        $manager->getManager()->flush();
+        return $this->redirectToRoute('app_get_all_author');
+
+    }
+    #[Route('/author/delete/{id}',name:'app_delete_author')]
+    public function delete($id,ManagerRegistry $manager,AuthorRepository $repo){
+        $author = $repo->find($id);
+        $manager->getManager()->remove($author);
+        $manager->getManager()->flush();
+        return $this->redirectToRoute('app_get_all_author');
+
     }
 }
---
+++ symfony3a11/src/Entity/Author.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuthorRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AuthorRepository::class)]
+class Author
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $email = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $nb_class = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getNbClass(): ?int
+    {
+        return $this->nb_class;
+    }
+
+    public function setNbClass(?int $nb_class): static
+    {
+        $this->nb_class = $nb_class;
+
+        return $this;
+    }
+}
